2bd90f188dddfa29d6b20c9d5f1bd88890a1d553
Asingopaul <115646873+Asingopaul@users.noreply.github.com>
2025-06-01 22:18:48 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "first_instance" {
+
+  ami           = "ami-00f34bf9aeacdf007"
+  instance_type = "t3.micro"
+
+  tags = {
+    name = "web-server"
+
+  }
+}
+
+resource "aws_vpc" "name" {
+
+    
+  
+}
